3ff16f91728f098f97eea2dfb002ed255dab4043
Tobie <khoichudang1@gmail.com>
2024-07-30 10:10:54 +0700

    deloy

--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -1,23 +1,26 @@
 import React from 'react'
 import ProjectItem from './ProjectItem'
 import img1 from '../assets/CMC-glitch.png'
-import img2 from '../assets/handholding.81456b15234d.png'
+import img2 from '../assets/ketqua.PNG'
 import img3 from '../assets/Screenshot_2024-04-12_123334.88dfb5a59cd2.png'
 import img4 from '../assets/Capture.PNG'
+const href2 = 'https://github.com/tobiCD/LSTM-ANKHE'
+const href1 = 'https://github.com/tobiCD/JWT_backEnd'
+const href3 = 'https://github.com/tobiCD/Blog-APP'
+const href4 = 'https://github.com/tobiCD/JobSearching'
 
 const Project=()=>{
 return (
     <div id='projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'
     >
         <h1 className='text-4xl font-bold text-center text-[#001b5e]'> Projects</h1>
-        <p className='text-center py-8'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum cupiditate nam quibusdam similique qui veritatis quia perspiciatis. Expedita et corporis unde, rerum, 
-            repellendus velit accusantium ut quo fugiat inventore voluptatibus.
+        <p className='text-center py-8'>
         </p>
         <div className='grid sm:grid-cols-2 gap-12'>
-            <ProjectItem image={img1} title='Beautiful'/>
-            <ProjectItem image={img3} title ='AI Reconigzed'/>
-            <ProjectItem image ={img3} title ='Simon Game'/>
-            <ProjectItem image ={img4} title ='Blog App'/>
+            <ProjectItem image={img1} href={href1} title='User Management -NodeJS'/>
+            <ProjectItem image={img2} href={href2} title ='An Khe River Flow Forecasting'/>
+            <ProjectItem image ={img3} href={href3} title ='Blog App-Django'/>
+            <ProjectItem image ={img4} href={href4} title ='Job Searching with React'/>
         </div>
         </div>
   )
--- src/component/ProjectItem.jsx
+++ src/component/ProjectItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function ProjectItem({image, title}) {
+function ProjectItem({image, title,href}) {
   return (
     <div className='relative flex items-center 
     justify-center h-auto w-full shadow-xl 
@@ -10,7 +10,7 @@ function ProjectItem({image, title}) {
         <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
             <h3 className='text-2xl font-bold text-white tracking-wider text-center'>{title}</h3>
             <p className='pb-4 pt-2 text-white text-center'>Some thing news?</p>
-            <a href="/">
+            <a href={href}>
                 <p className='text-center p-3 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg'>More Info</p>
 
             </a>
